e44e860e586f54b0f68e555eada1d23ea7a9a1fe
VsevolodMalashenko <vsevolod.malashenko@gmail.com>
2024-06-21 01:37:38 +0300

    TechTask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import BookList from './components/BookList';
+import Loader from './components/Loader';
+import { API_KEY } from './config';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
+  const [queryParams, setQueryParams] = useState({
+    query: '',
+    category: 'all',
+    sort: 'relevance',
+    startIndex: 0,
+  });
+
+  const fetchBooks = async (newQueryParams) => {
+    const { query, category, sort, startIndex } = newQueryParams;
+    const categoryQuery = category !== 'all' ? `+subject:${category}` : '';
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}${categoryQuery}&orderBy=${sort}&startIndex=${startIndex}&maxResults=30&key=${API_KEY}`;
+
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setBooks((prevBooks) =>
+        startIndex === 0 ? data.items : [...prevBooks, ...data.items],
+      );
+      setTotalItems(data.totalItems || 0);
+      setQueryParams(newQueryParams);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = (searchParams) => {
+    fetchBooks({ ...searchParams, startIndex: 0 });
+  };
+
+  const loadMoreBooks = () => {
+    fetchBooks({ ...queryParams, startIndex: queryParams.startIndex + 30 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <SearchBar onSearch={handleSearch} />
+      {loading && <Loader />}
+      <div className='results-info'>
+        {totalItems > 0 && <p>Found {totalItems} results</p>}
+      </div>
+      {Array.isArray(books) && <BookList books={books} />}
+      {totalItems > (books?.length || 0) && (
+        <button onClick={loadMoreBooks} className='load-more'>
+          Load more
+        </button>
+      )}
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'AIzaSyD20XrsTMjSlUR9Nqn0vmw79BKaY7cTesM'
